dcb678715c66dac086203c8d4e022e4a410497a4
Denis <benato.denis96@gmail.com>
2024-02-21 20:01:47 +0100

    Use RogPlatform to detect an asus-wmi platform

--- src/performance/gpu/amd/asus.rs
+++ src/performance/gpu/amd/asus.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::{path::Path, sync::{Arc, Mutex}};
 use udev::{Enumerator, Device};
 
 use crate::performance::gpu::tdp::{TDPDevice, TDPResult, TDPError};
@@ -7,7 +7,7 @@ use zbus::{Connection, Result};
 
 use rog_dbus::RogDbusClientBlocking;
 use rog_dbus::DbusProxies;
-use rog_platform::error::PlatformError;
+use rog_platform::{platform::RogPlatform, error::PlatformError};
 use rog_platform::platform::{GpuMode, Properties, ThrottlePolicy};
 use rog_profiles::error::ProfileError;
 
@@ -15,52 +15,25 @@ use rog_profiles::error::ProfileError;
 /// Implementation of asusd with a fallback to asus-wmi sysfs
 /// See https://www.kernel.org/doc/html/v6.8-rc4/admin-guide/abi-testing.html#abi-sys-devices-platform-platform-ppt-apu-sppt
 pub struct ASUS {
-    //ppt_pl1_spl: String,
-    //ppt_pl2_sppt: String,
+    platform: Arc<Mutex<RogPlatform>>,
 }
 
 impl ASUS {
 
     /// test if we are in an asus system with asus-wmi loaded
     pub fn new() -> Option<Self> {
-        /*
-        //let context = Context::new().expect("Failed to create udev context");
-        let mut enumerator = Enumerator::new(/*&context*/).expect("Failed to create enumerator");
-        enumerator.match_subsystem("platform").expect("Failed to add subsystem filter");
-        enumerator.match_sysname("asus-nb-wmi").expect("Failed to add sysname filter");
-
-        for device in enumerator.scan_devices().expect("Failed to scan devices") {
-            let ppt_pl1_spl = device.property_value("ppt_pl1_spl");
-            let ppt_pl2_sppt = device.property_value("ppt_pl2_sppt");
-
-            if let (Some(ppt_pl1_spl), Some(ppt_pl2_sppt)) = (ppt_pl1_spl, ppt_pl2_sppt) {
-                let ppt_pl1_spl_str = ppt_pl1_spl.to_str().unwrap().to_string();
-                let ppt_pl2_sppt_str = ppt_pl2_sppt.to_str().unwrap().to_string();
-
-                log::info!("Found asus-wmi module");
-                log::info!("ASUS ppt_pl1_spl: {}", ppt_pl1_spl_str);
-                log::info!("ASUS ppt_pl2_sppt: {}", ppt_pl2_sppt_str);
-
-                return Some(Self {
-                    ppt_pl1_spl: ppt_pl1_spl_str,
-                    ppt_pl2_sppt: ppt_pl2_sppt_str,
-                });
-            }
-        }
-        */
-
-        match Device::from_syspath(std::path::Path::new("/sys/devices/platform/asus-nb-wmi/")) {
-            Ok(udev) => {
+        match RogPlatform::new() {
+            Ok(platform) => {
                 log::info!("Module asus-wmi WAS found");
-                Some(Self {})
+                Some(Self {
+                    platform: Arc::new(Mutex::new(platform))
+                })
             },
             Err(err) => {
-                log::info!("Module asus-wmi not found");
+                log::info!("Module asus-wmi not found: {}", err);
                 None
             }
         }
-
-        
     }
 
 }
